7de37a75b7a600b143d03907acdfe71035ba3b01
beltram.maldant <beltram.maldant@soprabanking.com>
2018-11-09 09:27:41 +0100

    works without gradle dependencyManagement block

--- src/test/kotlin/com/beltram/springkt130/NormalTest.kt
+++ src/test/kotlin/com/beltram/springkt130/NormalTest.kt
@@ -1,14 +1,11 @@
 package com.beltram.springkt130
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
 typealias GreetingFunction = (String) -> Unit
-
-@SpringBootTest
-class SpringKt130ApplicationTests {
-    data class Person(val name: String, val log: GreetingFunction)
-    fun sayHello(name: String) = println("Hello $name")
+data class Person(val name: String, val log: GreetingFunction)
+internal class NormalTest {
+    private fun sayHello(name: String) = println("Hello $name")
     @Test fun `this works`() { Person("Beltram") { m -> sayHello(m) } }
     @Test fun `strangely this works`() { Person("Beltram", ::sayHello) }
     @Test fun `this fails`() { Person("Beltram", this::sayHello) }
